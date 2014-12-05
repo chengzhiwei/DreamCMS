0ac5c2a55a4bb127056c27118e15ae2a698e77d1
chengzhiwei <hot12121212@gmail.com>
2014-12-05 17:18:51 +0800

    插件模块
    插件模块

---
+++ Template/Admin/bootstrap/Plugin/Plugin/pluginlist.php
@@ -0,0 +1,78 @@
+<style>
+   .profile-activity .tools{display:block;}
+</style>
+<div class="page-content">
+    <div class="page-header">
+        <h1>
+
+            <small>
+                <i class="icon-double-angle-right"></i>
+                插件列表
+            </small>
+        </h1>
+    </div><!-- /.page-header -->
+
+    <div class="row">
+        <div class="col-xs-12">
+            <!-- PAGE CONTENT BEGINS -->
+
+
+            <div class="widget-body">
+                <div class="widget-main padding-8">
+                    <div  ><div class="profile-feed" id="profile-feed-1" >
+                            
+                            <div class="profile-activity clearfix">
+                                <div>
+                                    <i class="pull-left thumbicon icon-ok btn-success no-hover"></i>
+                                    <a href="#" class="user"> 百度编辑器UEditor </a>
+                                    作者：DreamCMS EMAIL：<a href="mailto">hot12121212@163.com</a>
+
+                                   
+                                    <div class="time">
+                                      UEditor 是一套开源的在线HTML编辑器,主要用于让用户在网站上获得所见即所得编辑效果
+                                        
+                                    </div>
+                                </div>
+
+                                <div class="tools action-buttons">
+                                    <a class="btn btn-xs btn-success" href="#">
+                                        安装
+                                    </a>
+                                    <a class="btn btn-xs btn-danger" href="#">
+                                        删除
+                                    </a>
+                                    
+                                </div>
+                            </div>
+
+                            <div class="profile-activity clearfix">
+                                <div>
+                                    <i class="pull-left thumbicon icon-picture btn-info no-hover"></i>
+                                    <a href="#" class="user"> WebUploader 文件上传插件 </a>
+                                    作者：DreamCMS EMAIL：<a href="mailto">hot12121212@163.com</a>
+
+                                    <div class="time">
+                                       
+                                        WebUploader是由Baidu WebFE(FEX)团队开发的一个简单的以HTML5为主,FLASH为辅的现代文件上传组件
+                                    </div>
+                                </div>
+
+                                <div class="tools action-buttons" >
+                                     <a class="btn btn-xs btn-yellow" href="#">
+                                        配置
+                                    </a>
+                                    <a class="btn btn-xs btn-danger" href="#">
+                                        卸载
+                                    </a>
+                                </div>
+                            </div>
+
+      
+                        </div>
+                        </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
